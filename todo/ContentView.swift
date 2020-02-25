2d97460c98f50ddfeb0f76fbaa332e401d2ec2f4
Kelven Galvão <kelvengalvao@gmail.com>
2020-02-25 17:24:56 -0300

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Kelven Irvine Borges Galvão on 25/02/20.
+//  Copyright © 2020 Kelven Irvine Borges Galvão. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
